3b568e34e93880fba70b48e63322c82e096ef230
priyapawarrao <priya.pawar30@gmail.com>
2014-09-10 23:04:12 -0700

    Lab1_HelloWorld

---
+++ src/main/scala/hello/HelloConfig.scala
@@ -0,0 +1,26 @@
+package hello
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, ResponseBody, RestController}
+/**
+* This config class will trigger Spring @annotation scanning and auto configure Spring context.
+*
+* @author saung
+* @since 1.0
+*/
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@RestController
+@RequestMapping(value=Array("/"))
+class HelloConfig {
+
+@RequestMapping(method = Array(RequestMethod.GET))
+@ResponseBody
+def respondtorequest(): String = {
+return "Hello World"
+
+}
+}
